59519f1c227e4a338a41b958eeb4a50643dfc0d8
kwasiasare <kwasi.spreadcom@gmail.com>
2024-12-09 10:07:59 -0500

    added authentication

--- src/index.js
+++ src/index.js
@@ -4,13 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// index.js
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_M4MZn2Nke",
+  client_id: "2o4iidvugug5stnj5hvqjopg89",
+  redirect_uri: "https://dev-env.d35xgk4ok41v85.amplifyapp.com/",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
