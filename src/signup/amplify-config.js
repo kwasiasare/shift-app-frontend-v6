2a3f68d4ca437f15bf97e45f4bc44f61e9b06b39
kwasiasare <104875131+kwasiasare@users.noreply.github.com>
2025-01-05 15:50:38 -0500

    Delete src directory

--- src/signup/amplify-config.js
+++
@@ -1,4 +0,0 @@
-import { Amplify } from 'aws-amplify';
-import awsExports from './amplify-output.json'; // Ensure this file is generated when you set up Amplify CLI or Studio
-
-Amplify.configure(awsExports);
