da102f3f2bfc1c085715b7a91118cb9571fa63c1
kwasiasare <kwasi.spreadcom@gmail.com>
2025-01-26 16:52:23 -0500

    update

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import { useAuth } from "react-oidc-context";
 import { useNavigate, useLocation } from "react-router-dom";
 import LogoutPage from "./components/LogoutPage";  // Import the LogoutPage component
 import { Route, Routes } from "react-router-dom"; // Import routing components
-import { cognitoConfig } from "./components/Config"; // Import Cognito session info
+import { cognitoConfig } from "./components/Cognito"; // Import Cognito session info
 
 // Custom theme
 const theme = createTheme({
